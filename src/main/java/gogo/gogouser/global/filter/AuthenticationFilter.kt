5558a562552de986f8b6683048c4bb74ee88e908
신희성 <131235625+tlsgmltjd@users.noreply.github.com>
2025-04-30 14:30:11 +0900

    Revert "[user] auth filter 로직 수정"

--- src/main/java/gogo/gogouser/global/filter/AuthenticationFilter.kt
+++ src/main/java/gogo/gogouser/global/filter/AuthenticationFilter.kt
@@ -1,43 +1,41 @@
 package gogo.gogouser.global.filter
 
-import gogo.gogouser.global.security.CustomUserDetailsService
+import gogo.gogouser.domain.user.persistence.Authority
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
-import org.springframework.security.authentication.AccountStatusUserDetailsChecker
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
 
 @Component
-class AuthenticationFilter(
-    private val userDetailsService: CustomUserDetailsService
-) : OncePerRequestFilter() {
-
+class AuthenticationFilter : OncePerRequestFilter() {
     override fun doFilterInternal(
         request: HttpServletRequest,
         response: HttpServletResponse,
         filterChain: FilterChain
     ) {
-        val userId = request.getHeader("Request-User-Id")
+        val userId: String? = request.getHeader("Request-User-Id")
+        val authority: Authority? = request.getHeader("Request-User-Authority")?.run { Authority.valueOf(this) }
 
-        if (userId == null) {
+        if (userId == null || authority == null) {
             filterChain.doFilter(request, response)
             return
         }
 
-        try {
-            val userDetails: UserDetails = userDetailsService.loadUserByUsername(userId)
-            AccountStatusUserDetailsChecker().check(userDetails)
+        val authorities: MutableCollection<SimpleGrantedAuthority> = ArrayList()
+        authorities.add(SimpleGrantedAuthority("ROLE_${authority.name}"))
+        val userDetails: UserDetails = User(userId, "", authorities)
 
-            val authentication = UsernamePasswordAuthenticationToken(userDetails, null, userDetails.authorities)
-            SecurityContextHolder.getContext().authentication = authentication
+        val authentication: Authentication =
+            UsernamePasswordAuthenticationToken(userDetails, "", userDetails.authorities)
+        SecurityContextHolder.getContext().authentication = authentication
 
-            filterChain.doFilter(request, response)
-        } catch (ex: Exception) {
-            response.sendError(HttpServletResponse.SC_FORBIDDEN, ex.message ?: "인증 실패")
-        }
+        filterChain.doFilter(request, response)
     }
 }
